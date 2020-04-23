1d93d01b9ad2b11ee8e0a457ede31e870129a03b
Michaela <michaela.j.kane@gmail.com>
2020-04-23 17:12:12 +0900

    click sky to make single star that moves to random location

---
+++ Assets/Scripts/Constellation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constellation : MonoBehaviour {
+
+    public GameObject star;
+    public int numStars = 1; // number of stars to spawn
+
+    void Start()
+    {
+    }
+
+    // call on click
+    void OnMouseDown()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            for (int i = 0; i < numStars; i++)
+            {
+                Instantiate(star, hit.point, Quaternion.identity);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/star.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class star : MonoBehaviour
+{
+    public Vector3 initPos; // initial position of the star
+    public Vector3 targetPos; // target position of the star
+    public int size; // size of the star
+    public float velocity = 100.0f; // initial velocity of the star
+
+    // called just once for script
+    void Awake()
+    {
+        initPos = transform.position;
+        Debug.Log("initPos: " + initPos);
+        targetPos = new Vector3(Random.Range(-5.0f, 5.0f), 
+            Random.Range(-5.0f, 5.0f), 10);
+        Debug.Log("targetPos: " + targetPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = velocity * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, step);
+    }
+}
